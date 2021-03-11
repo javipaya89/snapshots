e55b233bbf2b3ff4967c7e9088ebdf97471942d2
javi <javi@gestionate.es>
2021-03-11 06:11:06 +0100

    Second

---
+++ app/src/main/java/javipaya/com/snapshots/HomeFragment.kt
@@ -0,0 +1,158 @@
+package javipaya.com.snapshots
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.core.SnapshotHolder
+import javipaya.com.snapshots.databinding.FragmentHomeBinding
+import javipaya.com.snapshots.databinding.ItemSnapshotBinding
+import java.util.Objects.toString
+
+class HomeFragment : Fragment() {
+
+    private lateinit var mBinding: FragmentHomeBinding
+
+    private lateinit var mFirebaseAdapter: FirebaseRecyclerAdapter<Snapshot, SnapshotHolder>
+    private lateinit var mLayoutManager: RecyclerView.LayoutManager
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        Log.i("Firebase","onCreateView")
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.i("Firebase","onViewCreated")
+        val query = FirebaseDatabase.getInstance().reference.child("snapshots")
+
+        Log.d("Firebase",query.toString())
+
+        val options =
+            FirebaseRecyclerOptions.Builder<Snapshot>().setQuery(query) {
+                val snapshot = it.getValue(Snapshot::class.java)
+                snapshot!!.id = it.key!!
+                snapshot
+            }.build()
+
+
+        mFirebaseAdapter = object : FirebaseRecyclerAdapter<Snapshot, SnapshotHolder>(options){
+            private lateinit var mContext: Context
+
+
+
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SnapshotHolder {
+
+                Log.i("Firebase","onCreateViewHolder")
+
+                mContext = parent.context
+
+
+                val view = LayoutInflater.from(mContext)
+                    .inflate(R.layout.item_snapshot, parent, false)
+                return SnapshotHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: SnapshotHolder, position: Int, model: Snapshot) {
+                val snapshot = getItem(position)
+                Log.i("Firebase","onBindViewHolder")
+
+                with(holder){
+                    setListener(snapshot)
+
+                    binding.tvTitle.text = snapshot.title
+                    binding.cbLike.text = snapshot.likeList.keys.size.toString()
+                    /*FirebaseAuth.getInstance().currentUser?.let {
+                        binding.cbLike.isChecked = snapshot.likeList
+                            .containsKey(it.uid)
+                    }*/
+                    Glide.with(mContext)
+                        .load(snapshot.photoUrl)
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .centerCrop()
+                        .into(binding.imgPhoto)
+                }
+            }
+
+            override fun onDataChanged() {
+                super.onDataChanged()
+                mBinding.progressBar.visibility = View.GONE
+            }
+
+            override fun onError(error: DatabaseError) {
+                super.onError(error)
+                Toast.makeText(mContext, error.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        mLayoutManager = LinearLayoutManager(context)
+
+        mBinding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = mLayoutManager
+            adapter = mFirebaseAdapter
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mFirebaseAdapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mFirebaseAdapter.stopListening()
+    }
+
+    /*override fun goToTop() {
+        mBinding.recyclerView.smoothScrollToPosition(0)
+    }*/
+
+    private fun deleteSnapshot(snapshot: Snapshot){
+        val databaseReference = FirebaseDatabase.getInstance().reference.child("snapshots")
+        databaseReference.child(snapshot.id).removeValue()
+    }
+
+    /*private fun setLike(snapshot: Snapshot, checked: Boolean){
+        val databaseReference = FirebaseDatabase.getInstance().reference.child("snapshots")
+        if (checked){
+            databaseReference.child(snapshot.id).child("likeList")
+                .child(FirebaseAuth.getInstance().currentUser!!.uid).setValue(checked)
+        } else {
+            databaseReference.child(snapshot.id).child("likeList")
+                .child(FirebaseAuth.getInstance().currentUser!!.uid).setValue(null)
+        }
+    }*/
+
+    inner class SnapshotHolder(view: View) : RecyclerView.ViewHolder(view){
+        val binding = ItemSnapshotBinding.bind(view)
+
+        fun setListener(snapshot: Snapshot){
+            binding.btnDelete.setOnClickListener { deleteSnapshot(snapshot) }
+
+            binding.cbLike.setOnCheckedChangeListener { compoundButton, checked ->
+                //setLike(snapshot, checked)
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/javipaya/com/snapshots/Snapshot.kt
@@ -0,0 +1,10 @@
+package javipaya.com.snapshots
+
+import com.google.firebase.database.Exclude
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class Snapshot(@get:Exclude var id: String = "",
+                    var title: String = "",
+                    var photoUrl: String ="",
+                    var likeList: Map<String, Boolean> = mutableMapOf())
